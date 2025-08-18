06c9fd9f4806bb90e0a343415be8c3ded64c9bad
notnawfal <muhammed_nawfal@yahoo.com>
2025-08-18 02:56:04 +0100

    backend set up done

---
+++ backend_springboot/backend/src/main/java/com/gymai/backend/GymAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.gymai.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymAiBackendApplication.class, args);
+	}
+
+}
